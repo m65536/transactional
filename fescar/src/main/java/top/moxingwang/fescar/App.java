d386577e671f37829ff64bf73adab43584b68f99
m65536 <$mx@github#159>
2019-01-12 16:10:20 +0800

    11

---
+++ fescar/src/main/java/top/moxingwang/fescar/App.java
@@ -0,0 +1,13 @@
+package top.moxingwang.fescar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
